404fe41ae094eb38ecac82bb28fcc69600a63f65
cassian237 <cassian.lemans@gmail.Com>
2023-06-08 21:44:14 +0200

    Show qr code

--- app/src/main/java/not/a/bug/pocketv/ui/component/AuthScreen.kt
+++ app/src/main/java/not/a/bug/pocketv/ui/component/AuthScreen.kt
@@ -3,15 +3,21 @@ package not.a.bug.pocketv.ui.component
 import android.content.Intent
 import android.net.Uri
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.tv.material3.Button
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.Text
+import net.glxn.qrgen.android.QRCode
 import not.a.bug.pocketv.model.NetworkResult
 import not.a.bug.pocketv.viewmodel.AuthViewModel
 
@@ -21,12 +27,12 @@ fun AuthScreen(
     viewModel: AuthViewModel
 ) {
     val requestTokenResult by viewModel.requestTokenResult.collectAsState()
-    val openWebViewEvent = viewModel.openWebViewEvent.collectAsState(null)
-    val context = LocalContext.current
+    val displayQrCode by viewModel.displayQrCode.collectAsState(initial = "")
 
     when (requestTokenResult) {
         is NetworkResult.Success -> {
         }
+
         is NetworkResult.Error -> {
             val error = (requestTokenResult as NetworkResult.Error).exception.message
             Text(text = "Erreur de demande du jeton de requête : $error")
@@ -38,6 +44,7 @@ fun AuthScreen(
         is NetworkResult.Loading -> {
             CircularProgressIndicator()
         }
+
         else -> {
             Button(onClick = { viewModel.requestToken() }) {
                 Text(text = "Se connecter avec Pocket")
@@ -45,12 +52,11 @@ fun AuthScreen(
         }
     }
 
-    LaunchedEffect(openWebViewEvent.value) {
-        val url = openWebViewEvent.value
-        if (url != null) {
-            val customTabsIntent = CustomTabsIntent.Builder().build()
-            customTabsIntent.intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            customTabsIntent.launchUrl(context, Uri.parse(url))
-        }
+    if (displayQrCode.isNotEmpty()) {
+        Image(
+            bitmap = QRCode.from(displayQrCode).bitmap().asImageBitmap(),
+            contentDescription = null,
+            modifier = Modifier.size(200.dp)
+        )
     }
 }
--- app/src/main/java/not/a/bug/pocketv/viewmodel/AuthViewModel.kt
+++ app/src/main/java/not/a/bug/pocketv/viewmodel/AuthViewModel.kt
@@ -12,6 +12,7 @@ import not.a.bug.pocketv.model.NetworkResult
 import not.a.bug.pocketv.model.PocketUser
 import not.a.bug.pocketv.model.RequestTokenResponse
 import not.a.bug.pocketv.repository.PocketRepository
+import java.util.UUID
 import javax.inject.Inject
 
 @HiltViewModel
@@ -25,8 +26,8 @@ class AuthViewModel @Inject constructor(
     private val _authorizeResult = MutableStateFlow<NetworkResult<PocketUser>?>(null)
     val authorizeResult: StateFlow<NetworkResult<PocketUser>?> = _authorizeResult
 
-    private val _openWebViewEvent = MutableSharedFlow<String>()
-    val openWebViewEvent: SharedFlow<String> = _openWebViewEvent
+    private val _displayQrCode = MutableSharedFlow<String>()
+    val displayQrCode: SharedFlow<String> = _displayQrCode
 
     fun requestToken() {
         viewModelScope.launch {
@@ -35,22 +36,22 @@ class AuthViewModel @Inject constructor(
 
             if (requestTokenResult is NetworkResult.Success) {
                 val requestToken = requestTokenResult.data.code
-                openWebView("https://getpocket.com/auth/authorize?request_token=${requestToken}&redirect_uri=pocketapp107637I")
+                val deviceId = UUID.randomUUID().toString()
 
+                _displayQrCode.emit("https://pocketv-6ebcf.web.app?request_token=${requestToken}&key=$deviceId")
+                listenToAuthSuccess(deviceId)
             }
         }
+    }
+
+    private fun listenToAuthSuccess(id: String) {
+
     }
 
     // Appelé lorsque l'utilisateur revient à l'application après l'autorisation Pocket
     fun handleAuthorizationCallback(code: String) {
         viewModelScope.launch {
             _authorizeResult.value = pocketRepository.authorize(code)
-        }
-    }
-
-    private fun openWebView(url: String) {
-        viewModelScope.launch {
-            _openWebViewEvent.emit(url)
         }
     }
 }
